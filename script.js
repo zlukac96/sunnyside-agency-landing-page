239e38108104d11bc5c0ebbb475baf80f729cbba
zlukac96 <97309080+zlukac96@users.noreply.github.com>
2022-01-09 02:52:07 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
-/*var menuContainer = document.getElementById("menuContainer");
+var navMenu = document.getElementById("navMenu");
 var hamburgerButton = document.getElementById("hamburgerButton");
-var menuIcon = document.getElementById("menuIcon");
-var closeIcon = document.getElementById("closeIcon");
 
 function openCloseMenu() {
 
-	if(menuContainer.style.right === "0px"){
-		menuContainer.style.right = "";
-		closeIcon.style.display = "";
-		menuIcon.style.display = "";
+	if(navMenu.style.display === "flex"){
+		navMenu.style.display = "";
 	}
 	else {
-		menuContainer.style.right = "0px";
-		closeIcon.style.display = "block";
-		menuIcon.style.display = "none";
+		navMenu.style.display = "flex";
 	}
 }
 
 
-hamburgerButton.addEventListener('click', openCloseMenu);*/
+hamburgerButton.addEventListener('click', openCloseMenu);
